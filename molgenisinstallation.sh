985c745ef0e5c011cb1ddc933ac3531f978d2f41
AdiMac <adamsken1221@gmail.com>
2015-09-01 11:08:20 +0200

    removed files from master branch

--- molgenisinstallation.sh
+++
@@ -1,4 +0,0 @@
-# /bin/sh
-
-http://search.maven.org/remotecontent?filepath=org/molgenis/molgenis-app/1.4.2/molgenis-app-1.4.2.war
-http://search.maven.org/remotecontent?filepath=org/molgenis/molgenis-app/1.8.2/molgenis-app-1.8.2.war
