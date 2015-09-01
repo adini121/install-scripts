54e8b3556097eec1a717134df781d648e22a18ad
AdiMac <adamsken1221@gmail.com>
2015-09-01 11:09:02 +0200

    removed files from master

--- setenv.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-export CATALINA_OPTS="-Xmx2g -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -Dmolgenis.home=~/.molgenis/omx"
-export JRE_HOME="/usr/lib/jvm/java-8-oracle/jre/"
-#export CLASSPATH="/home/adi/tomcat/TomcatInstance8080/lib/mysql-connector-java-5.1.36-bin.jar"
-
