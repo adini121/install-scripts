b285b142f888d97f69e2f26f983fad52d0e79e66
AdiMac <adamsken1221@gmail.com>
2015-09-02 10:20:16 +0200

    added exit; line 32

--- moodle/moodle_installation.sh
+++ moodle/moodle_installation.sh
@@ -33,6 +33,7 @@ moodleDBsettings(){
 	CREATE DATABASE moodle_$MoodleVersion DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 	GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,CREATE TEMPORARY TABLES,DROP,INDEX,ALTER ON moodle_$MoodleVersion.* TO ‘moodleuser’@’localhost’ IDENTIFIED BY 'moodlepassword';
 	EOF
+	exit;
 }
 
 createMoodleHome(){
