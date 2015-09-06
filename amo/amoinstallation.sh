dc5657e2c8081b464e076d83f3bb2bbf690aaf99
AdiMac <adamsken1221@gmail.com>
2015-09-06 18:15:01 +0200

    further amo changes

--- amo/amoinstallation.sh
+++ amo/amoinstallation.sh
@@ -91,45 +91,45 @@ if [  ! -f /home/$user/AMOHome/$amoInstance/local_settings.py ];
         		touch /home/$user/AMOHome/$amoInstance/local_settings.py
         		chmod 755 /home/$user/AMOHome/$amoInstance/local_settings.py
         		echo "created local_settings.py"
-        		cat > /home/$user/AMOHome/$amoInstance/local_settings.py << EOF
-               	#local_settings.py 
-				#specify the settings for each AMO instance
-				from settings import *  # noqa
+cat > /home/$user/AMOHome/$amoInstance/local_settings.py << EOF
+#local_settings.py 
+#specify the settings for each AMO instance
+from settings import *  # noqa
 
-				#Database settings
-				DATABASE_URL = os.environ.get('DATABASE_URL','mysql://amouser:amopassword@localhost/amo_$dbName')
-				DATABASES = {'default': dj_database_url.parse(DATABASE_URL)}
-				DATABASES['default']['OPTIONS'] = {'init_command': 'SET storage_engine=InnoDB','sql_mode': 'STRICT_ALL_TABLES'}
-				DATABASES['default']['TEST_CHARSET'] = 'utf8'
-				DATABASES['default']['TEST_COLLATION'] = 'utf8_general_ci'
+#Database settings
+DATABASE_URL = os.environ.get('DATABASE_URL','mysql://amouser:amopassword@localhost/amo_$dbName')
+DATABASES = {'default': dj_database_url.parse(DATABASE_URL)}
+DATABASES['default']['OPTIONS'] = {'init_command': 'SET storage_engine=InnoDB','sql_mode': 'STRICT_ALL_TABLES'}
+DATABASES['default']['TEST_CHARSET'] = 'utf8'
+DATABASES['default']['TEST_COLLATION'] = 'utf8_general_ci'
 
-				#Memcached
-				CACHES = {
- 				   'default': {
-				        'BACKEND': 'caching.backends.memcached.MemcachedCache',
-				        'LOCATION': os.environ.get('MEMCACHE_LOCATION', 'localhost:'$memcachedPort''),
- 				   }
-				}
+#Memcached
+CACHES = {				
+ 	'default': {			   
+		'BACKEND': 'caching.backends.memcached.MemcachedCache',		        
+ 		'LOCATION': os.environ.get('MEMCACHE_LOCATION', 'localhost:'$memcachedPort''),				        
+		 		}
+		}
 
-				# Elasticsearch
-				ES_HOSTS = [os.environ.get('ELASTICSEARCH_LOCATION', '127.0.0.1:9200')]
-				ES_URLS = ['http://%s' % h for h in ES_HOSTS]
-				ES_INDEXES = {
-				    'default': 'addons_'$amoInstance'',
- 				   'stats': 'addons_'$amoInstance'_stats',
-				}
+# Elasticsearch
+ES_HOSTS = [os.environ.get('ELASTICSEARCH_LOCATION', '127.0.0.1:9200')]
+ES_URLS = ['http://%s' % h for h in ES_HOSTS]
+ES_INDEXES = {
+			'default': 'addons_'$amoInstance'',
+ 			'stats': 'addons_'$amoInstance'_stats',
+			}
 
-				# Celery
-				BROKER_URL = os.environ.get('BROKER_URL',
-                            'amqp://olympia:olympia@localhost:5672/'$amoInstance'')
+# Celery
+BROKER_URL = os.environ.get('BROKER_URL',
+            'amqp://olympia:olympia@localhost:5672/'$amoInstance'')
 
-				REDIS_LOCATION = os.environ.get('REDIS_LOCATION', 'localhost:'$redisPort'')
-				REDIS_BACKENDS = {
-    				'master': 'redis://{location}?socket_timeout=0.5'.format(
-        				location=REDIS_LOCATION)}
+REDIS_LOCATION = os.environ.get('REDIS_LOCATION', 'localhost:'$redisPort'')
+REDIS_BACKENDS = {
+    			'master': 'redis://{location}?socket_timeout=0.5'.format(
+        		location=REDIS_LOCATION)}
 EOF
         		echo "exiting local_settings.py"
-        fi
+fi
 }
 
 
