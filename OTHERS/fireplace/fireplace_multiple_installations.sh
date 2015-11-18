4ae4f02001e384ecc72c673f73d3ba12b8276032
AdiMac <adamsken1221@gmail.com>
2015-11-18 20:10:15 +0100

    Fireplace: Install multiple versions through OTHERS/fireplace_multiple_installations.sh

---
+++ OTHERS/fireplace/fireplace_multiple_installations.sh
@@ -0,0 +1,97 @@
+#! /bin/bash
+
+echo "____________________starting tmux session 2014_08_05_____________________"
+tmux kill-session -t fireplace
+tmux new -d -A -s fireplace_2014_08_05 '                                                                                                                                                                                              
+/home/nisal/scripts/install-scripts/fireplace/fireplaceinstallation.sh -u nisal -t 2014.08.05 -m first -p 8001
+tmux detach'
+
+echo "____________________starting tmux session _____________________"
+tmux kill-session -t fireplace 
+tmux new -d -A -s fireplace 2014_09_02'                                                                                                                                                                                              
+/home/nisal/scripts/install-scripts/fireplace/fireplaceinstallation.sh -u nisal -t 2014.09.02 -m second -p 8002
+tmux detach'
+
+echo "____________________starting tmux session _____________________"
+tmux kill-session -t fireplace 
+tmux new -d -A -s fireplace 2014_10_07'                                                                                                                                                                                              
+/home/nisal/scripts/install-scripts/fireplace/fireplaceinstallation.sh -u nisal -t 2014.10.07 -m third -p 8003
+tmux detach'
+
+echo "____________________starting tmux session _____________________"
+tmux kill-session -t fireplace 
+tmux new -d -A -s fireplace 2014_11_04'                                                                                                                                                                                              
+/home/nisal/scripts/install-scripts/fireplace/fireplaceinstallation.sh -u nisal -t 2014.11.04 -m fourth -p 8004
+tmux detach'
+
+echo "____________________starting tmux session _____________________"
+tmux kill-session -t fireplace 
+tmux new -d -A -s fireplace 2014_12_02'                                                                                                                                                                                              
+/home/nisal/scripts/install-scripts/fireplace/fireplaceinstallation.sh -u nisal -t 2014.12.02 -m fifth -p 8005
+tmux detach'
+
+echo "____________________starting tmux session _____________________"
+tmux kill-session -t fireplace 
+tmux new -d -A -s fireplace 2015_01_06'                                                                                                                                                                                              
+/home/nisal/scripts/install-scripts/fireplace/fireplaceinstallation.sh -u nisal -t 2015.01.06 -m sixth -p 8006
+tmux detach'
+
+echo "____________________starting tmux session _____________________"
+tmux kill-session -t fireplace 
+tmux new -d -A -s fireplace 2015_02_10'                                                                                                                                                                                              
+/home/nisal/scripts/install-scripts/fireplace/fireplaceinstallation.sh -u nisal -t 2015.02.10 -m eighth -p 8008
+tmux detach'
+
+echo "____________________starting tmux session _____________________"
+tmux kill-session -t fireplace 
+tmux new -d -A -s fireplace 2015_03_03'                                                                                                                                                                                              
+/home/nisal/scripts/install-scripts/fireplace/fireplaceinstallation.sh -u nisal -t 2015.03.03 -m ninth -p 8009
+tmux detach'
+
+echo "____________________starting tmux session _____________________"
+tmux kill-session -t fireplace 
+tmux new -d -A -s fireplace 2015_04_01'                                                                                                                                                                                              
+/home/nisal/scripts/install-scripts/fireplace/fireplaceinstallation.sh -u nisal -t 2015.04.01 -m tenth -p 9000
+tmux detach'
+
+echo "____________________starting tmux session _____________________"
+tmux kill-session -t fireplace 
+tmux new -d -A -s fireplace 2015_05_05'                                                                                                                                                                                              
+/home/nisal/scripts/install-scripts/fireplace/fireplaceinstallation.sh -u nisal -t 2015.05.05 -m eleventh -p 9001
+tmux detach'
+
+echo "____________________starting tmux session _____________________"
+tmux kill-session -t fireplace 
+tmux new -d -A -s fireplace 2015_06_09'                                                                                                                                                                                              
+/home/nisal/scripts/install-scripts/fireplace/fireplaceinstallation.sh -u nisal -t 2015.06.09 -m twelveth -p 9002
+tmux detach'
+
+echo "____________________starting tmux session _____________________"
+tmux kill-session -t fireplace 
+tmux new -d -A -s fireplace 2015_07_07'                                                                                                                                                                                              
+/home/nisal/scripts/install-scripts/fireplace/fireplaceinstallation.sh -u nisal -t 2015.07.07 -m thirteenth -p 9003
+tmux detach'
+
+echo "____________________starting tmux session _____________________"
+tmux kill-session -t fireplace 
+tmux new -d -A -s fireplace 2015_08_04'                                                                                                                                                                                              
+/home/nisal/scripts/install-scripts/fireplace/fireplaceinstallation.sh -u nisal -t 2015.08.04 -m fourteenth -p 9004
+tmux detach'
+
+echo "____________________starting tmux session _____________________"
+tmux kill-session -t fireplace 
+tmux new -d -A -s fireplace 2015_09_01'                                                                                                                                                                                              
+/home/nisal/scripts/install-scripts/fireplace/fireplaceinstallation.sh -u nisal -t 2015.09.01 -m fifteenth -p 9005
+tmux detach'
+
+echo "____________________starting tmux session _____________________"
+tmux kill-session -t fireplace 
+tmux new -d -A -s fireplace 2015_10_06'                                                                                                                                                                                              
+/home/nisal/scripts/install-scripts/fireplace/fireplaceinstallation.sh -u nisal -t 2015.10.06 -m sixteenth -p 9006
+tmux detach'
+
+echo "____________________starting tmux session _____________________"
+tmux kill-session -t fireplace 
+tmux new -d -A -s fireplace 2015_11_02'                                                                                                                                                                                              
+/home/nisal/scripts/install-scripts/fireplace/fireplaceinstallation.sh -u nisal -t 2015.11.02 -m seventeenth -p 9007
+tmux detach'
