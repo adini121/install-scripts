78a9af0b4401523d6a756b24aa3aecb74dfdf2dd
AdiMac <adamsken1221@gmail.com>
2016-01-04 09:20:28 +0100

    Revert "Tomcat infinity jvm heap allocation"
    This reverts commit 083ca005bcc369afbea703b95304b021ba09cb3a.

--- tomcat/infinity_installtomcatnew.sh
+++ tomcat/infinity_installtomcatnew.sh
@@ -15,28 +15,22 @@ usage(){
         echo "  -c <connectorPort>      Tomcat connector port"
         exit 1
 }
-export tomcat_home=/home/$user/tomcat/TomcatInstance$startupPort
+
 tomcatInstanceCreation(){
 echo "tomcat instance creation script. Installing instance TomcatInstance$startupPort"
-if [ ! -d $tomcat_home ]; then
-	mkdir $tomcat_home
+if [ ! -d /home/$user/tomcat/TomcatInstance$startupPort ]; then
+	mkdir /home/$user/tomcat/TomcatInstance$startupPort
 else 
 	echo "Tomcat instance TomcatInstance$startupPort already exists"
 fi
-        cp -R /home/$user/tomcat/apache-tomcat-7.0.63/* $tomcat_home
-        sleep 2
+        cp -R /home/$user/tomcat/apache-tomcat-7.0.63/* /home/$user/tomcat/TomcatInstance$startupPort
 }
 changingTomcatConf() {
-touch $tomcat_home/bin/setenv.sh
-chmod 777 $tomcat_home/bin/setenv.sh
-cat > $tomcat_home/bin/setenv.sh << EOF
-JAVA_OPTS="-Xms128m -Xmx512m"
-EOF
-sed -i 's|<Connector port=\"8080\"|<Connector port=\"'$startupPort'\"|g' $tomcat_home/conf/server.xml
-sed -i 's|<Server port=\"8005\"|<Server port=\"'$shutdownPort'\"|g' $tomcat_home/conf/server.xml
-sed -i 's|<Connector port=\"8009\"|<Connector port=\"'$connectorPort'\"|g' $tomcat_home/conf/server.xml
-sed -i 's|redirectPort=\"8443\"|redirectPort=\"8443\" URIEncoding=\"UTF-8\"/>|g' $tomcat_home/conf/server.xml
-$tomcat_home/bin/startup.sh
+sed -i 's|<Connector port=\"8080\"|<Connector port=\"'$startupPort'\"|g' /home/$user/tomcat/TomcatInstance$startupPort/conf/server.xml
+sed -i 's|<Server port=\"8005\"|<Server port=\"'$shutdownPort'\"|g' /home/$user/tomcat/TomcatInstance$startupPort/conf/server.xml
+sed -i 's|<Connector port=\"8009\"|<Connector port=\"'$connectorPort'\"|g' /home/$user/tomcat/TomcatInstance$startupPort/conf/server.xml
+sed -i 's|redirectPort=\"8443\"|redirectPort=\"8443\" URIEncoding=\"UTF-8\"/>|g' /home/$user/tomcat/TomcatInstance$startupPort/conf/server.xml
+/home/$user/tomcat/TomcatInstance$startupPort/bin/startup.sh
 }
 
 
