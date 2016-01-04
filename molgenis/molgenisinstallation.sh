c5fae2ce500b6abcbad6ec5a0edd633c46e10e97
AdiMac <adamsken1221@gmail.com>
2016-01-04 11:10:03 +0100

    Molgenis for infinity fixes

--- molgenis/molgenisinstallation.sh
+++ molgenis/molgenisinstallation.sh
@@ -8,13 +8,13 @@ usage(){
         echo "  -s <startupPort>        Tomcat startup port (e.g. 8082)"
         exit 1
 }
-
-molgenis_tomcat_home=/home/$user/tomcat/TomcatInstance$startupPort
-molgenis_home=/home/$user/.molgenis/omx
+echo "USER is : $user"
+TomcatHome=/home/$user/tomcat/TomcatInstance$startupPort
+MolgenisHome=/home/$user/.molgenis/omx
 
 molgenisDatabaseCreation(){
-if [ ! -f $molgenis_tomcat_home/lib/mysql-connector-java-5.1.24.jar ]; then
-    wget http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.24/mysql-connector-java-5.1.24.jar -P $molgenis_tomcat_home/lib
+if [ ! -f $TomcatHome/lib/mysql-connector-java-5.1.24.jar ]; then
+    wget http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.24/mysql-connector-java-5.1.24.jar -P $TomcatHome/lib
 fi
 mysql -u root << EOF
 DROP DATABASE IF EXISTS omx;
@@ -25,15 +25,15 @@ EOF
 }
 
 molgenisHomeConfiguration() {
-if [ -d $molgenis_home ]; then
-    rm -rf $molgenis_home
+if [ -d $MolgenisHome ]; then
+    rm -rf $MolgenisHome
 fi
 
-mkdir -p $molgenis_home;
+mkdir -p $MolgenisHome;
 
-touch $molgenis_home/molgenis-server.properties
-chmod 777 $molgenis_home/molgenis-server.properties
-cat > $molgenis_home/molgenis-server.properties << EOF
+touch $MolgenisHome/molgenis-server.properties
+chmod 777 $MolgenisHome/molgenis-server.properties
+cat > $MolgenisHome/molgenis-server.properties << EOF
 db_user=root
 db_password=
 db_uri=jdbc\:mysql\://localhost/omx
@@ -43,34 +43,34 @@ EOF
 }
 
 molgenisTomcatConfiguration(){
-rm -rf $molgenis_tomcat_home/webapps/*
-cp /home/$user/MolgenisWarFiles/molgenis"$molgenisVersion".war $molgenis_tomcat_home/webapps/ROOT.war
+rm -rf $TomcatHome/webapps/*
+cp /home/$user/MolgenisWarFiles/molgenis"$molgenisVersion".war $TomcatHome/webapps/ROOT.war
 sleep 10
-sed -i 's|.*CATALINA_OPTS=.*|CATALINA_OPTS=\"-Xmx2g -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -Dmolgenis.home=/home/$user/.molgenis/omx"|g' $molgenis_tomcat_home/bin/catalina.sh
-# sed -i '' 's|redirectPort=\"8443\"|redirectPort=\"8443\" maxPostSize=\"33554432\" scheme=\"https\" proxyPort=\"443\" URIEncoding=\"UTF-8\"/>|g' $molgenis_tomcat_home/conf/server.xml
-rm -f $molgenis_tomcat_home/logs/catalina.out
+sed -i 's|.*CATALINA_OPTS=.*|CATALINA_OPTS=\"-Xmx2g -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -Dmolgenis.home=/home/$user/.molgenis/omx"|g' $TomcatHome/bin/catalina.sh
+# sed -i '' 's|redirectPort=\"8443\"|redirectPort=\"8443\" maxPostSize=\"33554432\" scheme=\"https\" proxyPort=\"443\" URIEncoding=\"UTF-8\"/>|g' $TomcatHome/conf/server.xml
+rm -f $TomcatHome/logs/catalina.out
 export CATALINA_OPTS="-Xmx2g -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -Dmolgenis.home=/home/$user/.molgenis/omx"
 sleep 5
-$molgenis_tomcat_home/bin/shutdown.sh
+$TomcatHome/bin/shutdown.sh
 sleep 5
-$molgenis_tomcat_home/bin/startup.sh
+$TomcatHome/bin/startup.sh
 }
 
 
 while getopts ":u:m:s:" i; do
-        case "${i}" in
+    case "${i}" in
         u) user=${OPTARG}
         ;;
         m) molgenisVersion=${OPTARG}
         ;;
         s) startupPort=${OPTARG}
-        esac
+    esac
 done
 
 shift $((OPTIND - 1))
 
 if [[ $user == "" || $molgenisVersion == "" || $startupPort == "" ]]; then
-        usage
+    usage
 fi
 
 #..........................................function calls...................................
